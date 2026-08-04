65f71ec56fae760dd819941895f44448575c41df
PycMono <837706069@qq.com>
2026-08-04 17:47:46 +0800

    ✨ feat: 新增 ping.go 提供简单的 HTTP ping 接口

--- ping.go
+++ ping.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
-// pingHandler 返回一个简单的 JSON 响应，用于健康检查
 func pingHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{
-		"status": "ok",
-		"pong":   "pong",
-	})
+	fmt.Fprint(w, "pong")
 }
 
 func main() {
